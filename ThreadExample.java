ae60fbc282e2e2ec8006b13bc6cb682ec2581ba5
kiran <kiranvadlapally@gmail.com>
2022-03-18 13:51:55 +0530

    Added my ThreadUsingRunnable Class

--- ThreadExample.java
+++ ThreadExample.java
@@ -6,16 +6,10 @@ public class ThreadExample extends Thread{
     }
 
     public static void main(String[] args) {
-        ThreadExample t1=new ThreadExample();
-        ThreadExample t2=new ThreadExample();
-        ThreadExample t3=new ThreadExample();
-        ThreadExample t4=new ThreadExample();
-        ThreadExample t5=new ThreadExample();
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
+        for (int i=0;i<5;i++) {
+            ThreadExample t1 = new ThreadExample();
+            t1.start();
+        }
 
 
     }

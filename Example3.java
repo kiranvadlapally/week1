67f33cf9868bcf88c231d2d280e252f096b00350
kiran <kiranvadlapally@gmail.com>
2022-03-21 12:49:55 +0530

    Added my Class

---
+++ Example3.java
@@ -0,0 +1,18 @@
+//Java Program store group of integers(Order of inserting elements does not matter and store unique elements)
+import java.util.HashSet;
+public class Example3 {
+    public static void main(String[] args) {
+        HashSet<Integer> obj=new HashSet<>();
+        obj.add(5);
+        obj.add(4);
+        obj.add(9);
+        obj.add(1);
+        obj.add(3);
+        obj.add(5);
+        obj.add(4);
+        obj.add(9);
+        obj.add(1);
+        obj.add(3);
+        System.out.println("Set is "+obj);
+    }
+}

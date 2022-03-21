c8909ded7a6efb91814713ac176f53efffa5136b
kiran <kiranvadlapally@gmail.com>
2022-03-21 11:31:36 +0530

    Added my Class

---
+++ ArrayListDemo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1=new ArrayList<>();
+        list1.add(74);
+        list1.add(4);
+        list1.add(7);
+        list1.add(2);
+        list1.add(5);
+        list1.add(1);
+        list1.add(9);
+        list1.add(6);
+        System.out.println("List1 is "+list1);
+
+    }
+}
